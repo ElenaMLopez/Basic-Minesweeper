daf9ea0efaa82bf857525cc93829c1eb0cc07259
elenamlopez <elena.mateos1@gmail.com>
2021-11-05 09:54:01 +0100

    Add some styles

--- app.js
+++ app.js
@@ -52,8 +52,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if(i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('bomb')) total++;
         if(i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb')) total++;
         if(i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) total++;
-        if(i < 88 && !isRightEdge && squares[i +1 + width].classList.contains('bomb'))total++;
-        if(i < 89 && squares[i + width].classList.contains('bomb')) total++;
+        if(i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb'))total++;
+        if(i <= 89 && squares[i + width].classList.contains('bomb')) total++;
+        
+        if(i === 88 && squares[i + width + 1].classList.contains('bomb')) total++;
+        if(i === 98 && squares[i + 1].classList.contains('bomb')) total++;
 
         squares[i].setAttribute('data', total);
       }
